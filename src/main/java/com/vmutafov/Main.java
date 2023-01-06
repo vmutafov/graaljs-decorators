c0f9709ebd392dcb42b6e677fe653b5bd7b43eb5
Vladimir Mutafov <vlado.mutafov@gmail.com>
2023-01-06 14:43:48 +0200

    chore: reformat code

--- src/main/java/com/vmutafov/Main.java
+++ src/main/java/com/vmutafov/Main.java
@@ -10,26 +10,26 @@ public class Main {
                 .option("engine.WarnInterpreterOnly", "false")
                 .build();
 
-var js = """
-        
-        function logged(value, { kind, name }) {
-          if (kind === "method") {
-            return function (...args) {
-              console.log(`Calling ${name} with arguments ${args.join(", ")}`);
-              return value.call(this, ...args);
-            };
-          }
-        }
+        var js = """
                         
-        class DecoratedClass {
-          @logged
-          test(arg) {}
-        }
+                function logged(value, { kind, name }) {
+                  if (kind === "method") {
+                    return function (...args) {
+                      console.log(`Calling ${name} with arguments ${args.join(", ")}`);
+                      return value.call(this, ...args);
+                    };
+                  }
+                }
+                                
+                class DecoratedClass {
+                  @logged
+                  test(arg) {}
+                }
+                                
+                const decorated = new DecoratedClass();
+                decorated.test(123);
                         
-        const decorated = new DecoratedClass();
-        decorated.test(123);
-        
-        """;
+                """;
 
         context.eval("js", js);
     }
